1dc043af744c951449507f6317a96a2ee59ec33b
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-08-03 01:30:31 -0300

    feat: started the home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from "react";
 // import axios from "axios";
-import * as S from "./style";
+import * as S from "./style"; //importing all const exports from style contained within as S
+import { ReactComponent as Search } from "assets/imgs/icons/search.svg";
 import { header } from "components/header/style";
 
 const Home = () => {
@@ -18,9 +19,33 @@ const Home = () => {
 
   return (
     <S.home>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>MangaScans</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Put the date here
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Look for the flavor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Mangas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>List of mangas here</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Requests here</p>
+      </aside>
     </S.home>
   );
 };
